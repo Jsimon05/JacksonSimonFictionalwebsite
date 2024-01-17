c004241f3545819a9be365bbd4e914971efd5fea
Jsimon05 <jacksonxsimon@gmail.com>
2024-01-17 14:55:07 -0800

    updated form confirmation page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -19,6 +19,27 @@ def contact():
 @app.route('/products')
 def products():
     return render_template("products.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name =  request.args.get('name')
+    name = request.args.get('email')
+    name = request.args.get('phn')
+    name = request.args.get('billingaddress')
+    name = request.args.get('city')
+    name = request.args.get('state')
+    name = request.args.get('zipcode')
+    props = {
+        "name": name,
+        "email": email
+        "phn": phn
+        "billingaddress": billing address
+        "city": city
+        "state": state,
+        "zip code": zipcode
+    }
+
+    return render_template("confirmation.html", data=props)
 
 
 if __name__ == '__main__':
